b9088f76ce301e670b31b516df734a55f2c504ea
root <root@instance-2.c.root-app-210812.internal>
2018-09-17 01:38:42 +0800

    add

--- install.sh
+++ install.sh
@@ -2,8 +2,11 @@
 #
 # vim 配置文件自动化
 
+# 下载插件管理软件Vundle
 git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 
 cp vimrc ~/.vimrc
 
-
+# Install vim plug
+vim -c "PluginInstall" -c "q" -c "q"
+	
